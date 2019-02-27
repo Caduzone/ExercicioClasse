0df1ead723434b8116d7bc7a4f554008554a0795
Carlos Eduardo <cadu.dba000@gmail.com>
2019-02-27 08:18:12 -0300

    Initial Commit

---
+++ ExercicioClasse/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExercicioClasse
+//
+//  Created by Usuário Convidado on 27/02/19.
+//  Copyright © 2019 Carlos Eduardo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
